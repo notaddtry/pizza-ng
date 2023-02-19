070562531844f3069fac30b2a4f0282771fc2097
notaddtry <comatose.9999@mail.ru>
2023-02-19 19:01:13 +0300

    init app,adding header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer/footer.component';
+import { HeaderComponent } from './layout/header/header/header.component';
+import { MainComponent } from './layout/main/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
